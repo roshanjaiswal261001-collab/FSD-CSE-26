b515c2a863ea9cb40658b6ad450882f142012cae
Roshan <roshanjaiswal261001@gmail.com>
2026-08-25 10:24:04 +0530

    go

--- BOOK-CDN-APP/index.js
+++ BOOK-CDN-APP/index.js
@@ -1,13 +1,30 @@
-function Book(){
-    const image=React.createElement("img",{src:"",width:"100px",height:"100px"},null);
-    const title=React.createElement("h2",{color:"blue"},"Title: ReactJS");
-    const price=React.createElement("h2",{color:"green"},"Price: 450");
-    const btn= React.createElement("button",{color:"blue"},"Add To Cart");
+function Book(props){
+    const image=React.createElement("img",{src:props.image,width:"100px",height:"100px"},null);
+    const title=React.createElement("h2",{style:{color:"blue"}},"Title: " + props.title);
+    const price=React.createElement("h2",{style:{color:"green"}},"Price: " + props.price+ "/-");
+    const btn= React.createElement("button",{style:{color:"blue"}},"Add To Cart");
 
     const div= React.createElement("div",{className:"book"},[image,title,price,btn]);
+return div;
+}
+const bookdata=[
+    {image:"",tittle:"ReactJS",price:450},
+     {image:"",tittle:"NodeJS",price:539},
+      {image:"",tittle:"ExpresJS",price:250},
+       {image:"",tittle:"ReactJS",price:450},
+     {image:"",tittle:"NodeJS",price:539},
+      {image:"",tittle:"ExpresJS",price:250},
+]
+function App(){
+    const bookstore= bookdata.map((b)=>{
+        return Book(b);
+})
+const div= React.createElement("div",{className:"bookstore"},[...bookstore]
+
+)
 return div;
 }
 
 const parent= document.getElementById("root");
 const root= ReactDOM.createRoot(parent);
-root.render(Book());
+root.render(App());
